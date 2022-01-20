15ad10b53b6405e577eb8b8b879f56e252ed1b9b
Rauchendraser <andreasr@ppedv.de>
2022-01-20 16:40:12 +0100

    Spiegelung LogShipping Windows Cluster Übersischt

---
+++ SQL Hochverfügbarkeit/SQL Hochverfügbarkeit/00_Inventur.sql
@@ -0,0 +1,26 @@
+---INVENTUR
+
+
+/*
+
+Umgebung
+
+schulung\Administrator
+ppedv2019!
+
+RAM: 16 GB- 4 = 12
+1 Socket 
+4 logische 
+						       PRIV1			PRIV2           EXT    DBPFAD      BACKUPPFAD
+HV-DC   2048-3048  2 CPUs    192.168.137.1    192.168.137.2     172.   c:\_SQLDBS  C:\BACKUP
+
+HV-SQL1 4500  fix  4 CPUs    192.168.137.2	  192.168.138.2      -     c:\_SQLDBS  C:\BACKUP                                                                            
+
+HV-SQL2 4500 fix   4 CPUs    192.168.137.3	  192.168.138.3
+--NUMA KLick
+
+
+Dienstkonten: 
+DBModul schulung\svcSQL  ppedv2019!
+Agent:  schulung\sqlAgent ppedv2019!
+
